adbb35b91713f155b3e41f0ec9c0f1439822a0eb
Abiyyu Firmansyah <abiyyu.firmansyah@mncgroup.com>
2026-04-21 16:03:47 +0700

    Initial Commit

---
+++ BillMaster/BillMasterApp.swift
@@ -0,0 +1,32 @@
+//
+//  BillMasterApp.swift
+//  BillMaster
+//
+//  Created by Reza on 21/04/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct BillMasterApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
